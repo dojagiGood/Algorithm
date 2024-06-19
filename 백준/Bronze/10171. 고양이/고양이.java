e7a5426d75685742c0267379bdbbcbf13eee74e2
도쟈기굿 <108134661+dojagiGood@users.noreply.github.com>
2024-06-19 15:25:20 +0900

    [Bronze V] Title: 고양이, Time: 128 ms, Memory: 13936 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+
+public class Main {
+//첫 번째 줄에 A, B, C (1 ≤ A, B, C ≤ 10^12)이 공백을 사이에 두고 주어진다.
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println( " )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+        //"\"는 한개만 쓰면 출력안되고 \\2개를 써야 1개가 출력됨
+    }
+
+}
