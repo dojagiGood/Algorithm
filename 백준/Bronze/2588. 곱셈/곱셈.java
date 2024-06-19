f2e1ae1f4a7a7a71b3470a014f8fba4d27009bf8
도쟈기굿 <108134661+dojagiGood@users.noreply.github.com>
2024-06-19 14:59:37 +0900

    [Bronze III] Title: 곱셈, Time: 188 ms, Memory: 17664 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 public class Main {
-
+//2588번부터 다시 풀어보기
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
 
+        int A = scanner.nextInt();
+        String B = scanner.next();
+        scanner.close();
 
-        int c = a * (b % 10);
-        int d = a * ((b%100)/10);
-        int e = a * (b / 100);
-        int x = c + (d * 10) + (e * 100);
+        System.out.println(A * (B.charAt(2) - '0'));
+        System.out.println(A * (B.charAt(1) - '0'));
+        System.out.println(A * (B.charAt(0) - '0'));
 
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(e);
-        System.out.println(x);
+        //B.charAt(0) -'0' 이렇게하면 문자를 정수값으로 바꿀 수 있음
+        //char 형끼리 산술 연산은 int로 변환됨
 
+        System.out.println(A * Integer.parseInt(B));
+        //문자열을 정수로 만들어주는 메서드 
     }
 }
