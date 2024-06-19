27eae69cdc2b2bf1fe43d26de87dbfc195db6000
도쟈기굿 <108134661+dojagiGood@users.noreply.github.com>
2024-06-19 15:28:39 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,14 @@
+
+public class Main {
+//첫 번째 줄에 A, B, C (1 ≤ A, B, C ≤ 10^12)이 공백을 사이에 두고 주어진다.
+    public static void main(String[] args) {
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+
+        //"\"는 한개만 쓰면 출력안되고 \\2개를 써야 1개가 출력됨
+    }
+
+}
