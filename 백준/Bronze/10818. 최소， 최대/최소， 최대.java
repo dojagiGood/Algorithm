552edee5175325b645083702ddfb3225add4f05b
도쟈기굿 <108134661+dojagiGood@users.noreply.github.com>
2024-06-25 16:39:38 +0900

    [Bronze III] Title: 최소, 최대, Time: 508 ms, Memory: 92076 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,38 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i : arr) {
+            if (max < i) {
+                max = i;
+            }
+
+            if (min > i) {
+                min = i;
+            }
+        }
+
+        System.out.println(min + " " + max);
+    }
+
+}
